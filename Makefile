e17b2929caaa66c51a025ce3f80c036a4959d16a
Mathieu Lecarme <mlecarme@bearstech.com>
2022-03-27 00:13:46 +0100

    More tests. Better coverage

--- Makefile
+++ Makefile
@@ -7,5 +7,10 @@ build-linux:
 	make build GOOS=linux
 	if [ "upx not found" != "$(shell which upx)" ]; then upx bin/audisp; fi
 
+test:
+	go test -cover \
+		github.com/athoune/audisp-go/fmt \
+		github.com/athoune/audisp-go/audisp
+
 bin:
 	mkdir -p bin
---
+++ audisp/audisp_test.go
@@ -0,0 +1,45 @@
+package audisp
+
+import (
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAudisp(t *testing.T) {
+	folder := t.TempDir()
+	l, err := net.Listen("unix", folder+"/socket")
+	assert.NoError(t, err)
+	msg := make(chan string, 10)
+	stop := make(chan interface{})
+	go func() {
+		conn, err := l.Accept()
+		assert.NoError(t, err)
+		for {
+			select {
+			case m := <-msg:
+				conn.Write([]byte(m + "\n"))
+			case <-stop:
+				return // stop the goroutine
+			}
+		}
+	}()
+	a, err := New(folder + "/socket")
+	assert.NoError(t, err)
+	defer a.Close()
+	msg <- "app=boo"
+	msg <- "name='Bon Sinclar"
+	line, err := a.Line()
+	assert.NoError(t, err)
+	cpt := 0
+	keys := []string{"app", "name"}
+	for line.Next() {
+		assert.NoError(t, line.Error())
+		k, _ := line.KeyValue()
+		assert.Equal(t, keys[cpt], k)
+		cpt++
+	}
+	stop <- nil
+
+}
